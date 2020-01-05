6b2174485c516970d55916feae26375f5e7b4363
Joshua <psyjr4@nottingham.ac.uk>
2020-01-05 16:56:28 +0000

    Fixed error in image builder

--- restartHome.sh
+++ restartHome.sh
@@ -7,7 +7,7 @@ docker rm joshua-home
 echo "Build the new joshua-home-image"
 docker build -t root/joshua-home-image .
 echo "Running the new joshua-home container"
-docker run --name joshua-home--restart always -p 38117:8002 -d root/joshua-home-image
+docker run --name joshua-home --restart always -p 38117:8002 -d root/joshua-home-image
 echo ""
 
 echo "Complete"
